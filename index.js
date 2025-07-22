6e76c8d7e3d5ca63d607093d0db44e1f41d87cff
karenkarap <myspecspace@gmail.com>
2025-07-22 16:35:10 +0000

    Deploying to gh-pages from @ karenkarap/goit-js-hw-12@1ec3a874086f6d0e53ce0db4259bbeada6aeee7f 🚀

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import{a as p,S as m,i as n}from"./assets/vendor-CLAh-PDR.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const u=o=>p.get("https://pixabay.com/api/",{params:{key:"51377392-58695daed08c65a7357b6e761",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(r=>r.data),l={gallery:document.querySelector(".gallery"),loader:document.querySelector(".js-loader")},g=new m(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"}),f=o=>{const r=o.map(({webformatURL:a,largeImageURL:s,tags:e,likes:t,views:i,comments:c,downloads:d})=>`
+import{a as q,S as E,i as c}from"./assets/vendor-B7yatFIi.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const l of o.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&a(l)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();async function f(s,t,r){return(await q.get("https://pixabay.com/api/",{params:{key:"51377392-58695daed08c65a7357b6e761",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:r,page:t}})).data}const n={gallery:document.querySelector(".gallery"),loader:document.querySelector(".js-loader"),button:document.querySelector(".js-button-more")},P=new E(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"}),y=s=>{const t=s.map(({webformatURL:r,largeImageURL:a,tags:e,likes:o,views:l,comments:w,downloads:S})=>`
         <li class="gallery-item">
             <div class="card"> 
-                <a class="gallery-link" href="${s}">
-                    <img class="gallery-image" src="${a}" alt="${e}" />
+                <a class="gallery-link" href="${a}">
+                    <img class="gallery-image" src="${r}" alt="${e}" />
                 </a>
                 <div class="info">
                     <div class="info-row labels">
@@ -12,13 +12,13 @@ import{a as p,S as m,i as n}from"./assets/vendor-CLAh-PDR.js";(function(){const 
                         <p>Downloads</p>
                     </div>
                     <div class="info-row values">
-                        <p>${t}</p>
-                        <p>${i}</p>
-                        <p>${c}</p>
-                        <p>${d}</p>
+                        <p>${o}</p>
+                        <p>${l}</p>
+                        <p>${w}</p>
+                        <p>${S}</p>
                     </div>
                 </div>
             </div>
         </li>
-            `).join("");l.gallery.innerHTML=r,g.refresh()},y=()=>{l.gallery.innerHTML=""},h=()=>{l.loader.classList.remove("is-hidden")},L=()=>{l.loader.classList.add("is-hidden")},v={form:document.querySelector(".form"),gallery:document.querySelector(".gallery")},b=o=>{o.preventDefault();const r=o.target.elements["search-text"].value.trim();if(!r){n.warning({title:"Warning",message:"Search field cannot be empty.",position:"topRight"});return}y(),h(),u(r).then(a=>{const s=a.hits;s.length===0&&n.info({title:"No results",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),f(s)}).catch(a=>{console.error("Error:",a),n.error({title:"Error",message:"Failed to load images. Please try again.",position:"topRight"})}).finally(()=>{L()})};v.form.addEventListener("submit",b);
+            `).join("");n.gallery.insertAdjacentHTML("beforeend",t),P.refresh()},R=()=>{n.gallery.innerHTML=""},h=()=>{n.loader.classList.remove("is-hidden")},b=()=>{n.loader.classList.add("is-hidden")},L=()=>{n.button.classList.remove("is-hidden")},p=()=>{n.button.classList.add("is-hidden")},m={form:document.querySelector(".form"),gallery:document.querySelector(".gallery"),button:document.querySelector(".js-button-more")};let i=1;const g=15;let d=null,u=null;const v=async s=>{i+=1,h();try{const r=(await f(u,i,g)).hits;y(r);const{height:a}=document.querySelector(".gallery").firstElementChild.getBoundingClientRect();window.scrollBy({top:a*2+16,behavior:"smooth"}),i>=d?(p(),c.info({title:"End of results",message:"We're sorry, but you've reached the end of search results.",position:"topRight"}),m.button.removeEventListener("click",v)):L()}catch(t){console.log(t)}finally{b()}},$=async s=>{if(s.preventDefault(),p(),u=s.target.elements["search-text"].value.trim(),!u){c.warning({title:"Warning",message:"Search field cannot be empty.",position:"topRight"});return}R(),i=1,h();try{const t=await f(u,i,g),r=t.hits;if(d=Math.ceil(t.totalHits/g),r.length===0){c.info({title:"No results",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"});return}y(r),d>1&&(m.button.addEventListener("click",v),L()),i>=d&&(p(),c.info({title:"End of results",message:"We're sorry, but you've reached the end of search results.",position:"topRight"}))}catch(t){console.error("Error:",t),c.error({title:"Error",message:"Failed to load images. Please try again.",position:"topRight"})}finally{b()}};m.form.addEventListener("submit",$);
 //# sourceMappingURL=index.js.map
